d5ff0422175f04471b4e2fc16f295b2cc8150aa1
yqchange <yqchange0212@foxmail.com>
2016-04-03 12:37:16 +0800

    九九乘法表

---
+++ 乘法表.py
@@ -0,0 +1,8 @@
+for i in range(1,10):
+    for j in range(1, 10):
+        if j>=i:
+          formula = '{0:1}x{1:1}={2:<2}'.format(i, j, i*j)
+          print(formula, end=' ')
+        else:
+          print(end='       ')
+    print()
